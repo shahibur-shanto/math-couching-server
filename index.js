4b9e4d845e30b8b7821382b5572dce7624f77f88
shahibur.shanto <shahibur407@gmail.com>
2021-04-02 17:17:10 +0600

    booking

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db(`${process.env.DB_NAME}`).collection("products");
+  const boolCollection = client.db(`${process.env.DB_NAME}`).collection("booking");
   
   app.post('/addEvent',(req,res)=>{
     const newEvent = req.body;
@@ -23,6 +24,19 @@ client.connect(err => {
         res.send(result)
     })
   })
+
+
+
+
+  app.post('/booking',(req,res)=>{
+    const newEvent = req.body;
+    collection.insertOne(newEvent)
+    .then(result=>{
+        console.log('inserted count',result.insertedCount)
+        res.send(result)
+    })
+  })
+
 
   app.get('/products',(req,res)=>{
     collection.find({})
